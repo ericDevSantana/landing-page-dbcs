4a6d56d48c5e6f47e19b2fa82f1932927ff5b152
Eric Santana <ericsantana@Erics-Mac-mini.local>
2021-10-13 14:09:28 -0700

    content

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Dynamic Black Car Service</title>
-        <meta name="viewport"/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
